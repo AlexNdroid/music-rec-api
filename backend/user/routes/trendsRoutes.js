0a344504a95de91afa3b942a91152c072c2d2d1b
Alex <alexma4@gmail.com>
2025-12-20 15:06:46 +0100

    Añadido cambios finales en endpoint /api/trends

--- backend/user/routes/trendsRoutes.js
+++ backend/user/routes/trendsRoutes.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const trendingController  = require("../controllers/trendsController");
 
+// Endpoint temporal de prueba
+router.get("/test", (req, res) => {
+  res.json([
+    { _id: "1", title: "Canción 1", artist: "Artista 1", trendScore: 10 },
+    { _id: "2", title: "Canción 2", artist: "Artista 2", trendScore: 8 },
+  ]);
+});
+
+
 //================== Obtener tendencias =================
 router.get("/", trendingController.getTrending);
 
